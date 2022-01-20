5b64861e2e74ab0357a8a994ea8ff5be36ec52f9
Mina Migić <mmigic@smart.edu.rs>
2022-01-20 17:46:24 +0100

    Pocetni komit

---
+++ mina/Radnik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mina
+{
+    class Radnik
+    {
+        public string ime;
+        public string prezime;
+        public string jmbg;
+        public double zarada;
+
+        public Radnik(string ime, string prezime, string jmbg, double zarada)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.jmbg = jmbg;
+            this.zarada = zarada;
+        }
+
+        public virtual double godisnjaZ()
+        {
+            return zarada * 12;
+        }
+
+    }
+}
---
+++ mina/programer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mina
+{
+    class Programer:Radnik
+    {
+        public double bonus;
+       public Programer(string ime, string prezime, string jmbg, double zarada, double bonus):base(ime,prezime,jmbg,zarada)
+        {
+            this.bonus = bonus;
+        }
+
+        public override double godisnjaZ()
+        {
+            return base.godisnjaZ() + bonus / 100 * base.godisnjaZ();
+        }
+    }
+}
